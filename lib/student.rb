48b7005957858ff3c25564a921394840fe2d27ca
Jamal Frank <jamaljfrank@gmail.com>
2020-01-27 20:33:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,5 +57,16 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  
+  def self.find_by_grade(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
