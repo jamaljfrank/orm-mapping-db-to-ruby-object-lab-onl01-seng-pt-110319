4bb79a0d9c88028256e087deaf265c77e1bb9ec8
Jamal Frank <jamaljfrank@gmail.com>
2020-01-27 20:41:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,11 +84,11 @@ class Student
     end
   end
   
-  def self.students_below_12th_grade
+  def self.first_X_students_in_grade_10
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade < 12
+      WHERE grade = 12
     SQL
  
     DB[:conn].execute(sql).map do |row|
